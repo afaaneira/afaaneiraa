d6954c4b1acceca0985d565bf1359fd09a30f63e
afaaneira <afaaneira@gmail.com>
2025-05-22 09:12:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("chaningmurray")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "dimsthemeatguy"
 )
+st.image("IMG-20250508-WA0072.jpg")
